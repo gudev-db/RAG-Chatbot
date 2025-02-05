8ec5cdf5608decd069fbaad68f40fbc22798eb85
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-02-05 15:11:51 +0000

    new similarity metric: cosine

--- app/components/Bubble.tsx
+++ app/components/Bubble.tsx
@@ -1,8 +1,12 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
 const Bubble = ({ message }) => {
-    const { content, role } = message;
-    return(
-        <div className={`${role} bubble`}>{content}</div>
-    )
-}
+  return (
+    <div className="bubble">
+      <ReactMarkdown>{message.content}</ReactMarkdown>
+    </div>
+  );
+};
 
 export default Bubble;
