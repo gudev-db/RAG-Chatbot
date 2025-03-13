1782449eb413917688d4aa0ba50cbd5c99d407d4
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-03-13 10:25:09 +0000

    teste

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -44,37 +44,48 @@ const citrosuco = [
 ];
 
 // define which website to scrape
-const citrosuco2 = [
+const cocamar = [
 
   "https://docs.google.com/document/d/1tskVRU6wHZT9MrgwGQYxvOTavE-Bc6kvScsBtkbnAH4/edit?usp=sharing",
-  "https://www.holambra.com.br/",
-  "https://www.holambra.com.br/servicos/beneficiamento-armazenamento-e-transporte",
-  "https://www.holambra.com.br/servicos/creditos-e-investimento",
-  "https://www.holambra.com.br/carreiras",
-  "https://www.holambra.com.br/governanca/portal-de-privacidade",
-  "https://www.holambra.com.br/governanca/organograma-geral",
-  "https://www.holambra.com.br/governanca/conselho",
-  "https://www.holambra.com.br/produtos/culturas",
-  "https://www.holambra.com.br/produtos/racao",
-  "https://www.holambra.com.br/produtos/insumos-agricolas",
-  "https://www.holambra.com.br/unidades/matriz",
-  "https://www.holambra.com.br/unidades/avare",
-  "https://www.holambra.com.br/unidades/itabera",
-  "https://www.holambra.com.br/unidades/loja-itabera",
-  "https://www.holambra.com.br/unidades/loja-itapetininga",
-  "https://www.holambra.com.br/unidades/loja-santa-cruz-do-rio-pardo",
-  "https://www.holambra.com.br/unidades/sao-manuel",
-  "https://www.holambra.com.br/unidades/takaoka",
-  "https://www.holambra.com.br/unidades/taquari",
-  "https://www.holambra.com.br/unidades/taquarituba",
-  "https://www.holambra.com.br/unidades/taquarivai",
-  "https://www.holambra.com.br/institucional/quem-somos",
-  "https://www.holambra.com.br/institucional/linha-do-tempo",
-  "https://www.holambra.com.br/institucional/certificacoes-e-premiacoes",
-  "https://www.holambra.com.br/institucional/sustentabilidade-e-responsabilidade-social",
-  "https://www.holambra.com.br/institucional/inovacao",
-  "https://www.linkedin.com/company/cooperativa-agro-industrial-holambra/",
-  "https://cnpj.biz/60906724000120",
+  "https://pt.wikipedia.org/wiki/Maring%C3%A1",
+  "https://www.viajali.com.br/maringa-pontos-turisticos/",
+  "https://gmconline.com.br/",
+
+  "https://www.cocamar.com.br/sobre",
+  "https://www.cocamar.com.br/sobre/sustentabilidade",
+  "https://www.cocamar.com.br/sobre/instituto-cocamar",
+  "https://www.cocamar.com.br/sobre/estrutura-organizacional",
+  "https://www.cocamar.com.br/sobre/certificacoes",
+  "https://www.cocamar.com.br/onde-atuamos",
+  "https://www.cocamar.com.br/negocios/irrigacao",
+  "https://www.cocamar.com.br/negocios/graos",
+  "https://www.cocamar.com.br/negocios/nutricao-animal",
+  "https://www.cocamar.com.br/negocios/sementes-cocamar",
+  "https://www.cocamar.com.br/negocios/fertilizantes-viridian/5/viridian",
+  "https://www.cocamar.com.br/negocios/produtos-cocamar",
+  "https://www.cocamar.com.br/negocios/racoes-para-pets",
+  "https://www.cocamar.com.br/negocios/cocamar-maquinas",
+  "https://www.cocamar.com.br/negocios/industria-de-fios",
+  "https://www.cocamar.com.br/negocios/posto-cocamar",
+  "https://www.cocamar.com.br/negocios/energia-solar",
+  "https://www.cocamar.com.br/negocios/cocamartransportes",
+  "https://www.cocamar.com.br/negocios/madeira-tratada",
+  "https://www.cocamar.com.br/negocios/corretora-de-seguros",
+  "https://www.cocamar.com.br/negocios/carne",
+
+
+  "https://www.informecadastral.com.br/cnpj/cocamar-cooperativa-agroindustrial-79114450000165",
+
+
+  "https://docs.google.com/document/d/1jhfLliZ3xFPPRVujycHk7mZuGG8ZX_-I97GVEFFviaA/edit?tab=t.0",
+  "https://docs.google.com/document/d/1zGu1fNopzGBzf2OU7KFvAvsThtrFNnsWMlRMXkcQx0M/edit?tab=t.0",
+  "https://docs.google.com/document/d/1W823sGLeNPAlw44QtmsTej9BYgbCmepQrL83xFokcos/edit?tab=t.0",
+  "https://docs.google.com/document/d/19dpqC75jSDmkF6R5NbwecM3Yoj4ebOSiNbJ2zwL9cGE/edit?tab=t.0",
+
+  "https://docs.google.com/document/d/19sVar6ENkkXQ1mI94aUC905gOOcc9PRoos6eq-3L1Mk/edit?tab=t.0",
+  "https://docs.google.com/document/d/1o37Nt0DACvqGjflFdz2jy4eVIk6vK02g59xFM822_k0/edit?tab=t.0",
+  "https://docs.google.com/document/d/1-sKhHdqsSS53UPI2fNqFQmW85zEltf0V41bZ4VlCBPM/edit?tab=t.0",
+
 
   
   "https://docs.google.com/document/d/1LhENR1ISC6lmXsERcVva9S6LUovJuIMBFtCsk8OSe-Y/edit?tab=t.0",
@@ -84,6 +95,8 @@ const citrosuco2 = [
 
 ];
 
+const transcript =["https://docs.google.com/document/d/1LhENR1ISC6lmXsERcVva9S6LUovJuIMBFtCsk8OSe-Y/edit?tab=t.0",]
+
 // Comment: Not conforming to TS strictness
 const client = new DataAPIClient(ASTRA_DB_APPLICATION_TOKEN);
 const db = client.db(ASTRA_DB_API_ENDPOINT, { namespace: ASTRA_DB_NAMESPACE });
@@ -91,7 +104,7 @@ const db = client.db(ASTRA_DB_API_ENDPOINT, { namespace: ASTRA_DB_NAMESPACE });
 // set splitter + options
 const splitter = new RecursiveCharacterTextSplitter({
   chunkSize: 1024,
-  chunkOverlap: 180,
+  chunkOverlap: 360,
 });
 
 // create collection on datastrax from datastax API
@@ -111,7 +124,7 @@ const createCollection = async (
 // load sample data into collection
 const loadSampleData = async () => {
   const collection = await db.collection(ASTRA_DB_COLLECTION);
-  for await (const url of citrosuco2) {
+  for await (const url of cocamar) {
     const content = await scrapePage(url);
     const chunks = await splitter.splitText(content);
     for await (const chunk of chunks) {
